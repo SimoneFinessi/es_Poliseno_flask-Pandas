e5d9e8bd52b572b74a46eda246e457c0f0d4fd28
SimoneFinessi <finessi.simone@iisgalvanimi.edu.it>
2023-04-25 09:11:06 +0000

    fatto es3

--- app.py
+++ app.py
@@ -28,12 +28,24 @@ def ris1():
 #es2
 @app.route('/es2')
 def es2():
-    return render_template('es1.html',list=df["platform"].unique())
+    return render_template('es2.html')
 
 @app.route('/ris2', methods=["post"])
 def ris2():
-    piatta=request.form["piatt"]
-    trovato=df[df.platform.str.lower()==piatta.lower()].to_html()
+    mass=int(request.form["massimo"])
+    minimo=int(request.form["minimo"])
+    inmezzo=pd.DataFrame()
+    for i in range(len(df)):
+        data=df.loc[i].release_date[8:12]
+        if int(data)>=minimo and int(data)<=mass:
+            inmezzo=pd.concat([inmezzo,df.loc[i]],axis=1)
+    trovato=inmezzo.to_html()
+    return render_template('risultato.html',risultato=trovato)
+#es3
+@app.route('/es3')
+def es3():
+    lista=[int(df.loc[i].metascore + df.loc[i].user_score) for i in range(len(df)) ]
+    trovato=df[df.metascore + df.user_score==max(lista)].to_html()
     return render_template('risultato.html',risultato=trovato)
 
 if __name__ == '__main__':
